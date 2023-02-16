17f8a31bae1c36cad4087a8b806c9482f474a4ad
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-02-16 11:43:02 +0000

    added new images and updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,40 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProjectItem from "./ProjectItem";
-import PropertyImg from "/public/assets/projects/property.jpg";
-import CryptoImg from "/public/assets/projects/crypto.jpg";
+import MovieFinderImg from "/public/assets/projects/movies.png";
+import QuizImg from "/public/assets/projects/quiz.png";
 import NetflixImg from "/public/assets/projects/netflix.jpg";
 import TwitchImg from "/public/assets/projects/twitch.jpg";
 
 export default function Projects() {
   return (
     <section id="projects" className="w-full">
-      <div className="max-w-[1240px] mx-auto px-2 py-16">
+      <div className="max-w-[1240px] mx-auto px-6 py-16">
         <p className="uppercase text-xl tracking-widest text-[#FF7F50]">
           Projects
         </p>
         <h2 className="py-4">What I&apos;ve built</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title="Property Finder"
-            backgroundImg={PropertyImg}
-            projectUrl="/property"
+            title="Movie Finder"
+            backgroundImg={MovieFinderImg}
+            projectUrl="/MovieFinder"
           />
           <ProjectItem
-            title="Crypto"
-            backgroundImg={CryptoImg}
-            projectUrl="/crypto"
+            title="Quiz App"
+            backgroundImg={QuizImg}
+            projectUrl="/Quiz"
           />
-          <ProjectItem
+          {/* <ProjectItem
             title="Netflix Clone"
             backgroundImg={NetflixImg}
-            projectUrl="/netflix"
+            projectUrl="/Netflix"
           />
           <ProjectItem
             title="Twitch Clone"
             backgroundImg={TwitchImg}
-            projectUrl="/twitch"
-          />
+            projectUrl="/Twitch"
+          /> */}
         </div>
       </div>
     </section>
